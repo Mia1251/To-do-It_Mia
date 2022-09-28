bde98622b6733123bf9b5d852572cdda00cd816b
Mia1251 <svanteomax@gmail.com>
2022-09-28 12:28:51 +0200

    Add project files.

---
+++ To-do It_Mia/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace To_do_It_Mia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
