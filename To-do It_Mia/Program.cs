e3bb5342d62310e87a486a33784779e5c7917821
Mia1251 <svanteomax@gmail.com>
2022-10-04 16:26:54 +0200

    Tests left to do!

--- To-do It_Mia/Program.cs
+++ To-do It_Mia/Program.cs
@@ -8,12 +8,14 @@ namespace To_do_It_Mia
         {
             Console.WriteLine("Below are the details for person: " );
 
-            Person person1 = new Person(1, "Charles","");
-            //Person person2 = new Person(2, "Sayeh", "");
-            //Person person3 = new Person(3, "Maria", "Svensson");
-            Console.WriteLine(person1.PersonId + " " + person1.FirstName + " " + person1.LastName);
+            //Person person1 = new Person(1, "Maria", "Svensson");
+            //Person person2 = new Person(2, "Charles", "");
+            //Person person3 = new Person(3, "", "Owanta");
+
+            //Console.WriteLine(person1.PersonId + " " + person1.FirstName + " " + person1.LastName);
             //Console.WriteLine(person2.PersonId + " " + person2.FirstName + " " + person2.LastName);
             //Console.WriteLine(person3.PersonId + " " + person3.FirstName + " " + person3.LastName);
+ 
         }
     }
 }
